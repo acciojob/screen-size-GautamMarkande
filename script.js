8242a8d00b1b51c8ff50547870f02430ab43d516
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-01 19:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const divEle = document.createElement("div");
+divEle.id = "sizeInfo"
+window.resize  = function(e) {
+	let x = window.innerWidth;
+	let y = window.innerHeight;
+	divEle.innerHtml = `<h1>Width:${x} and Height:${y}`;
+	document.body.appendChild(divEle);
+}
+resize();
+	
+}
